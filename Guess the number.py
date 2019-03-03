5a687e14ae891e101ed2a6223e9c8e9ed4f30996
Ben Sharkey <bensharkey3@gmail.com>
2019-03-03 12:02:15 +1100

    changed file ref location

--- Guess the number.py
+++ Guess the number.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-code = 'https://raw.githubusercontent.com/bensharkey3/Guess-The-Number/master/Guess%20the%20number%20game.py'
+code = 'https://raw.githubusercontent.com/bensharkey3/Guess-The-Number/master/sourcecode.py'
 response = urllib.request.urlopen(code)
 data = response.read()
 exec(data)
